6fb9c629fb8c105136b3b6d45a72039916657c9d
yundonghui <dhsimpson@donnie4567.com>
2022-10-30 19:20:49 +0900

    feat | publish dis dir

---
+++ dist/components/Board.vue.d.ts
@@ -0,0 +1,20 @@
+import { PropType } from 'vue';
+import type { Col } from '@/shared/types/Col';
+declare const _default: import("vue").DefineComponent<{
+    boardList: ArrayConstructor;
+    columnList: PropType<Col[]>;
+    boardHead: StringConstructor;
+    boardRow: StringConstructor;
+    headerCol: StringConstructor;
+    rowCol: StringConstructor;
+}, unknown, unknown, {}, {
+    selectParam(data: any, col: Col): String;
+}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    boardList: ArrayConstructor;
+    columnList: PropType<Col[]>;
+    boardHead: StringConstructor;
+    boardRow: StringConstructor;
+    headerCol: StringConstructor;
+    rowCol: StringConstructor;
+}>>, {}>;
+export default _default;
