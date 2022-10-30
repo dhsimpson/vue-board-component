6d9f49ca299b92caee05197b75165e7936b59153
yundonghui <dhsimpson@donnie4567.com>
2022-10-30 23:39:26 +0900

    refactor | inline css to mounted hook

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,33 +1,12 @@
 <template>
-    <section style="
-        display: table;
-        width: 100%;
-        border-collapse: collapse;
-    ">
-        <header
-        style="display: table-row"
-         :class="[boardHead]">
-            <div style="
-                display: table-cell;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap
-            "
-            :style="{'max-width': data.maxWidth as string}"
-             :class="['col', headerCol]" v-for="(data, idx) in columnList" :key="idx">
+    <section id="board-section">
+        <header :class="['row', boardHead]">
+            <div :class="['col', headerCol]" :style="{'max-width': data.maxWidth as string}" v-for="(data, idx) in columnList" :key="idx">
                 {{data.name}}
             </div>
         </header>
-        <div style="display: table-row" 
-        :class="['row', boardRow]" v-for="(data, idx) in boardList" :key="idx">
-            <div style="
-                display: table-cell;
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap
-            "
-            :style="{'max-width': col.maxWidth as string}"
-            :class="['col', rowCol]" v-for="(col, col_idx) in columnList" :key="col_idx">
+        <div :class="['row', boardRow]" v-for="(data, idx) in boardList" :key="idx">
+            <div :class="['col', rowCol]" :style="{'max-width': col.maxWidth as string}" v-for="(col, col_idx) in columnList" :key="col_idx">
                 {{selectParam(data, col)}}
             </div>
         </div>
@@ -35,7 +14,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, useCssModule } from 'vue'
 import type { CSSProperties } from 'vue'
 import type {Col} from '@/shared/types/Col';
 
@@ -56,6 +35,29 @@ export default defineComponent({
           Object.keys(data).find(key => {if(param ==key) res = data[key]});
           return res;
       }
+  },
+  mounted() {
+      const boardSection = document.getElementById('board-section') as HTMLElement;
+      {
+          boardSection.style.display = 'table';
+          boardSection.style.width = '100%';
+          boardSection.style.borderCollapse = 'collapse';
+      }
+
+      const columnList = Array.from(document.getElementsByClassName('col') as HTMLCollectionOf<HTMLElement>);
+    
+      columnList.forEach((col: HTMLElement) => {
+          col.style.display = 'table-cell';
+          col.style.overflow = 'hidden';
+          col.style.textOverflow = 'ellipsis';
+          col.style.whiteSpace = 'nowrap';
+      });
+
+      const rowList = Array.from(document.getElementsByClassName('row') as HTMLCollectionOf<HTMLElement>);
+    
+      rowList.forEach((row: HTMLElement) => {
+          row.style.display = 'table-row';
+      });
   }
 })
 </script>
